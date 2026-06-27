858ca982685a6f7ac6886c89bb3109d3acf105a7
Ahmed Ibrahim <ahmed@sila.local>
2026-06-27 17:11:39 +0300

    fastssz: use official Sila gohashtree dependency

--- go.mod
+++ go.mod
@@ -1,18 +1,18 @@
 module github.com/sila-chain/fastssz
 
-go 1.19
+go 1.22
 
 require (
 	github.com/golang/snappy v0.0.3
 	github.com/minio/sha256-simd v1.0.0
 	github.com/mitchellh/mapstructure v1.3.2
-	github.com/sila-chain/gohashtree v0.0.0-20240129161530-f61e0ca8e562
 	golang.org/x/tools v0.1.8
 	gopkg.in/yaml.v2 v2.3.0
 )
 
 require (
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/sila-chain/gohashtree v0.0.0-20260627124905-8386602ecf40
 	golang.org/x/mod v0.5.1 // indirect
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
--- vendor/github.com/sila-chain/gohashtree/hash.go
+++ vendor/github.com/sila-chain/gohashtree/hash.go
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-# Copyright (c) 2021 Prysmatic Labs
+# Copyright (c) 2021-2025 Prysmatic Labs
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -28,8 +28,6 @@ import (
 	"unsafe"
 )
 
-func _hash(digests *byte, p [][32]byte, count uint32)
-
 // Hash hashes the chunks two at the time and outputs the digests on the first
 // argument. It does check for lengths on the inputs.
 func Hash(digests [][32]byte, chunks [][32]byte) error {
@@ -38,10 +36,10 @@ func Hash(digests [][32]byte, chunks [][32]byte) error {
 	}
 
 	if len(chunks)%2 == 1 {
-		return fmt.Errorf("odd number of chunks")
+		return ErrOddChunks
 	}
 	if len(digests) < len(chunks)/2 {
-		return fmt.Errorf("not enough digest length, need at least %v, got %v", len(chunks)/2, len(digests))
+		return fmt.Errorf("%w: need at least %v, got %v", ErrNotEnoughDigests, len(chunks)/2, len(digests))
 	}
 	if supportedCPU {
 		_hash(&digests[0][0], chunks, uint32(len(chunks)/2))
@@ -64,14 +62,17 @@ func HashByteSlice(digests []byte, chunks []byte) error {
 	if len(chunks) == 0 {
 		return nil
 	}
+
 	if len(chunks)%64 != 0 {
-		return fmt.Errorf("chunks not multiple of 64 bytes")
+		return ErrChunksNotMultipleOf64
 	}
+
 	if len(digests)%32 != 0 {
-		return fmt.Errorf("digests not multiple of 32 bytes")
+		return ErrDigestsNotMultipleOf32
 	}
+
 	if len(digests) < len(chunks)/2 {
-		return fmt.Errorf("not enough digest length, need at least %d, got %d", len(chunks)/2, len(digests))
+		return fmt.Errorf("%w: need at least %v, got %v", ErrNotEnoughDigests, len(chunks)/2, len(digests))
 	}
 	// We use an unsafe pointer to cast []byte to [][32]byte. The length and
 	// capacity of the slice need to be divided accordingly by 32.
